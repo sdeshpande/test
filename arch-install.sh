1cf42d85cdba35f2ae9dbd9318b2cef8b5c9e923
Saurabh N. Deshpande <sdeshpande@virtuallized.com>
2021-03-09 14:06:48 -0500

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -54,7 +54,7 @@ wipefs "${part_root}"
 
 mkfs.vfat -F32 "${part_boot}"
 mkswap "${part_swap}"
-mkfs.f2fs -f "${part_root}"
+mkfs.ext4 -f "${part_root}"
 
 swapon "${part_swap}"
 mount "${part_root}" /mnt
